18980567b7e453e76b30b83e1f063cf23c81deb7
Ronoox <konpontiko@gmail.com>
2025-09-08 10:59:29 +0300

    Add files via upload

---
+++ TTS/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace TTS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(951, 112);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(477, 647);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(59, 578);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Speak";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 12);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(359, 27);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Choose a voice!";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(59, 685);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(322, 74);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Pause/Resume";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(42, 126);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(535, 56);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(59, 473);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(322, 74);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reset text";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Choose the preferred volume form 0-100";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(33, 286);
+            this.trackBar2.Minimum = -10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(535, 56);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(490, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Choose the preferred speed from -10 (slowest) to 10 (fastest)";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(59, 361);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(359, 27);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.Text = "Choose from our stories!";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Location = new System.Drawing.Point(516, 473);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(322, 74);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Save story to DB";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1040, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 19);
+            this.label3.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1017, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 27);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(945, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Category:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(945, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 19);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Title:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button5.Location = new System.Drawing.Point(516, 578);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(322, 74);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Get an online story!";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button6.Location = new System.Drawing.Point(516, 685);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(322, 74);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Rate us!";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1742, 1053);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Storyteller";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ TTS/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace TTS
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        HttpClient client = new HttpClient();
+
+        String connectionString = "Data source = stories.db;Version=3";
+        SQLiteConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChangeColor.SetFormColor(this, Color.Gray);
+
+            foreach (InstalledVoice voice in synthesizer.GetInstalledVoices())
+            {
+                comboBox1.Items.Add(voice.VoiceInfo.Name);
+            }
+
+            comboBox2.Items.Add("--Aesop--");
+            comboBox2.Items.Add("A Raven And A Swan");
+            comboBox2.Items.Add("--Classics--");
+            comboBox2.Items.Add("Winter in the Boulevard");
+            comboBox2.Items.Add("--Kids--");
+            comboBox2.Items.Add("The Adventures of Sunny the Squirrel");
+            comboBox2.Items.Add("--Sci-Fi--");
+            comboBox2.Items.Add("The Signal");
+            comboBox2.Items.Add("--Winter--");
+            comboBox2.Items.Add("The Lost Christmas Star");
+
+            connection = new SQLiteConnection(connectionString);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            synthesizer.SpeakAsync(richTextBox1.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            synthesizer.SelectVoice(comboBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (synthesizer.State == SynthesizerState.Speaking)
+            {
+                synthesizer.Pause();
+            }
+            else if (synthesizer.State == SynthesizerState.Paused)
+            {
+                synthesizer.Resume();
+            }
+            else
+            {
+                MessageBox.Show("There is nothing playing at the moment! Try playing something.");
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            synthesizer.Volume = trackBar1.Value;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            label3.Text = "";
+            textBox1.Clear();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            synthesizer.Rate = trackBar2.Value;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem?.ToString() == "A Raven And A Swan")
+            {
+                richTextBox1.Text = "A Raven, which you know is black as coal, was envious of the Swan, because her feathers were as white as the purest snow. The foolish bird got the idea that if he lived like the Swan, swimming and diving all day long and eating the weeds and plants that grow in the water, his feathers would turn white like the Swan's.\n\nSo he left his home in the woods and fields and flew down to live on the lakes and in the marshes. But though he washed and washed all day long, almost drowning himself at it, his feathers remained as black as ever.And as the water weeds he ate did not agree with him, he got thinner and thinner, and at last he died.\n\nA change of habits will not alter nature.";
+                label3.Text = "Aesop";
+                textBox1.Text = "A  Raven And A Swan";
+            }
+            else if (comboBox2.SelectedItem?.ToString() == "Winter in the Boulevard")
+            {
+                richTextBox1.Text = "The frost has settled down upon the trees. And ruthlessly strangled off the fantasies. Of leaves that have gone unnoticed, swept like old. Romantic stories now no more to be told.\n\nThe trees down the boulevard stand naked in thought, their abundant summery wordage silenced, caught. In the grim undertow; naked the trees confront. Implacable winter's long, cross-questioning brunt.Has some hand balanced more leaves in the depths of the twigs? Some dim little efforts placed in the threads of the birch?\n\nIt is only the sparrows, like dead black leaves on the sprigs, Sitting huddled against the cerulean, one flesh with their perch.The clear, cold sky coldly bethinks itself.\n\nLike vivid thought the air spins bright, and all. Trees, birds, and earth, arrested in the after-thought. Awaiting the sentence out from the welkin brought.";
+                label3.Text = "Classics";
+                textBox1.Text = "Winter in the Boulevard";
+            }
+            else if (comboBox2.SelectedItem?.ToString() == "The Adventures of Sunny the Squirrel")
+            {
+                richTextBox1.Text = "Once upon a time in a peaceful forest, there lived a cheerful squirrel named Sunny. Sunny loved exploring the forest, always curious about the world beyond the tall trees. One day, while gathering acorns for winter, Sunny found a shiny golden nut stuck in the roots of an old oak tree.\r\n\r\nSunny tugged and pulled until the nut popped free, revealing a tiny door beneath it. The door opened, and out came a magical fairy! \"Thank you, Sunny! You’ve freed me from a long spell. In return, I’ll grant you one wish.\"\r\n\r\nSunny thought carefully. \"I wish for all the animals in the forest to have enough food for winter!\"\r\n\r\nThe fairy waved her wand, and suddenly, the trees were filled with nuts, berries, and fruits. All the animals cheered, thanking Sunny for their feast. From that day on, Sunny was known as the hero of the forest.";
+                label3.Text = "Kids";
+                textBox1.Text = "The Adventures of Sunny the Squirrel";
+            }
+            else if (comboBox2?.SelectedItem?.ToString() == "The Signal")
+            {
+                richTextBox1.Text = "In the year 3057, Earth had become a bustling hub of intergalactic civilizations. Maya, a young astrophysicist, worked at the Deep Space Listening Station. One evening, while analyzing data, Maya noticed a strange pattern in a distant galaxy—an intelligent signal repeating in prime numbers.\r\n\r\nMaya deciphered the signal and found coordinates for an unknown planet. Against orders, she joined a covert expedition to investigate. When the team landed, they discovered a city made of pure energy, inhabited by beings of light.\r\n\r\nThe beings communicated telepathically, revealing they sent the signal as a test to find peaceful civilizations. Impressed by Maya's courage and intelligence, they shared advanced technology to restore Earth's ecosystem.\n\nReturning home, Maya became a hero, proving that curiosity and bravery could unlock the universe’s greatest secrets.";
+                label3.Text = "Sci-Fi";
+                textBox1.Text = "The Signal";
+            }
+            else if (comboBox2.SelectedItem?.ToString() == "The Lost Christmas Star")
+            {
+                richTextBox1.Text = "It was Christmas Eve, and the little town of Hollyville was bustling with holiday cheer. But something was missing—the star atop the giant Christmas tree in the town square! Without the star, the tree looked incomplete, and everyone felt disappointed.\n\nEllie, a brave 10-year-old girl, decided to search for the missing star. With her trusty dog Max, she followed glittering footprints leading into the forest. They found a mischievous elf named Jingle, who had taken the star by mistake, thinking it was a magical toy.\n\nEllie explained how important the star was to the town. Feeling guilty, Jingle returned it, and together they placed it atop the tree just as the clock struck midnight. The star lit up the night sky, and everyone cheered. Santa himself appeared, praising Ellie for her determination.\n\nFrom that night on, Ellie’s bravery became part of Hollyville's Christmas legend.";
+                label3.Text = "Winter";
+                textBox1.Text = "The Lost Christmas Star";
+            }
+
+            string selectedItem = comboBox2.SelectedItem?.ToString();
+
+            if (selectedItem != null && selectedItem.StartsWith("--") && selectedItem.EndsWith("--"))
+            {
+                MessageBox.Show("You cannot select a category. Please choose a story.");
+                comboBox2.SelectedIndex = -1;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            String insert = "INSERT INTO Categories (Category, Title, Story) VALUES (@Category, @Title, @Story)";
+            SQLiteCommand command = new SQLiteCommand(insert, connection);
+            command.Parameters.AddWithValue("@Category", label3.Text);
+            command.Parameters.AddWithValue("@Title", textBox1.Text);
+            command.Parameters.AddWithValue("@Story", richTextBox1.Text);
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Story added to database!");
+            }
+            else
+            {
+                MessageBox.Show("There was an error!");
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = "https://shortstories-api.onrender.com";
+
+                try
+                {
+                    var response = await client.GetStringAsync(apiUrl);
+
+                    var storyData = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+                    storyData?.Remove("_id");
+
+                    textBox1.Text = storyData["title"];
+                    richTextBox1.Text = storyData["story"];
+                    label3.Text = "Aesop";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        public class RateButton
+        {
+            public void Redirect(string url)
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = url,
+                        UseShellExecute = true
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Σφάλμα κατά το άνοιγμα του συνδέσμου: {ex.Message}", "Σφάλμα", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            RateButton RateButton = new RateButton();
+            RateButton.Redirect("https://play.google.com/store/games?device=windows");
+        }
+    }
+}
+
+public class ChangeColor
+{
+    public static void SetFormColor(Form form, Color color)
+    {
+        if (form != null)
+        {
+            form.BackColor = color;
+        }
+    }
+}
